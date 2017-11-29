7a7bbd58a3b017fd6a458cb57d63c6c36cd228c7
David Nowinsky <david@nowinsky.net>
2017-11-29 14:11:59 +0100

    Clean up and put initial object

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>Toucan Object explorer</h1>
+    <h2>Test by <!-- put your name here --></h2>
+
   </div>
 </template>
 
@@ -24,7 +12,15 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      obj: {
+        super: {
+          prop: {
+            nested: 'reall'
+          },
+          other: 'hey'
+        },
+        stuff: 'yeah'
+      }
     }
   }
 }
